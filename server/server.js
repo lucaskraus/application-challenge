3f6836715378edf7738742d018df2b918fbbcca7
Lucas Kraus <lucas-kraus@hotmail.com>
2023-04-13 21:59:20 -0300

    Update server.js

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const axios = require('axios');
+const app = express ();
+
+app.use(express.json());
+
+app.listen(3001, () => {
+	console.log("Servidor rodando (3001)...");
+});
