35e8046389fc1667405ea83dad7715d3c85ffb41
Lucas Kraus <lucas-kraus@hotmail.com>
2023-04-16 20:31:40 -0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,22 +5,34 @@ import List from './components/List.js';
 import User from './components/User.js';
 import Repos from './components/Repos.js';
 import './index.css';
-import { Routes, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const routing = (
-  <Routes>
-    <Route path="/" element={<App />} >
-      <Route path="api/users" element={<List />} />
-      <Route path="api/users/:username/details" element={<User />} />
-      <Route path="api/users/:username/repos" element={<Repos />} />
-    </Route>
-  </Routes>
-);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/api/users",
+        element: <List />
+      },
+      {
+        path: "/api/users/:username/details",
+        element: <User />
+      },
+      {
+        path: "/api/users/:username/repos",
+        element: <Repos />
+      },
+        
+    ]
+  }
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   <React.StrictMode>
-    {routing}
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
