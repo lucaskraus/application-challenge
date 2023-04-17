89324baa7ae070533c3063e17814ceec81421182
Lucas Kraus <lucas-kraus@hotmail.com>
2023-04-16 22:04:20 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,6 +11,11 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 //API query to get users from Github.
 app.get('/api/users', async (req, res) => {
   try {
@@ -27,11 +32,6 @@ app.get('/api/users', async (req, res) => {
     res.status(500).send("Couldn't find the users list.");
   }
 });
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
 
 //API query to get details from an specific user.
 app.get('/api/users/:username/details', async (req, res) => {
